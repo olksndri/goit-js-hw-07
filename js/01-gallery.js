1084fd6533309e3a202d2ca2a86159ed9f58b924
Oleksandr Tymoshenko <hermeusmanua@gmail.com>
2023-04-23 12:54:50 +0300

    01-gallery done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// import * as basicLightbox from "basiclightbox";
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector("ul.gallery");
+const images = galleryItems.reduce((prevVal, elem) => {
+  prevVal += `<li class="gallery__item">
+  <a class="gallery__link" href="${elem.original}">
+  <img class="gallery__image" src="${elem.preview}" alt ="${elem.description}" data-source="${elem.original}"/>
+  </a></li>`;
+  return prevVal;
+}, "");
+
+gallery.innerHTML = images;
+
+gallery.addEventListener("click", imgScale);
+gallery.addEventListener("focusout", (event) => {
+  removeEscapeListener();
+});
+
+let instance = basicLightbox.create(``);
+
+function imgScale(event) {
+  event.preventDefault();
+  const imgSrc = event.target.dataset.source;
+  instance = basicLightbox.create(`
+    <img class="gallery__image" src="${imgSrc}" width="800" height="600"/>`);
+  instance.show();
+  addEscapeListener();
+}
+
+function addEscapeListener() {
+  document.addEventListener("keydown", escapeListener);
+  // console.log("add");
+}
+
+function removeEscapeListener() {
+  document.removeEventListener("keydown", escapeListener);
+  // console.log("removing");
+}
+
+function escapeListener(event) {
+  if (event.key.toLowerCase() === "escape") {
+    instance.close();
+    // console.log("listening...");
+  }
+}
+
+// const imgAlt = event.target.getAttribute("alt"); (function imgScale)
